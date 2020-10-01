5bb34a917b63da085f37d3b22dca77ddcea149e8
VakhoIosava <Vakhtangiiosava@gmail.com>
2020-10-01 22:10:22 +0400

    JavaHomework3

---
+++ src/vaxoiosava/classInt1.java
@@ -0,0 +1,14 @@
+package vaxoiosava;
+
+public class classInt1 {
+    protected int x = 4;
+    public classInt1() {
+        System.out.println("Hello My Class");
+    }
+    void even() {
+        if (x % 2 == 0) {
+            System.out.println("This number is even");
+        } else
+            System.out.println("This number is odd");
+    }
+}
